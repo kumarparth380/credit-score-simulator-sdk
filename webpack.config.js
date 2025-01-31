eefd2604626d86fbc82456db89fd7d2abe5e4c02
kumarparth380 <kumarparth380@gmail.com>
2025-01-31 14:28:57 +0530

    fix: webpack & tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: './src/index.js',          // Entry point for the SDK
   output: {
-    filename: 'sdk.js',
+    filename: 'bundle.js',           // Output bundle file
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',                 // Make sure the assets are served from the root
     library: 'CreditScoreSDK',
     libraryTarget: 'umd',
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],  // Handle file extensions
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.jsx?$/, // This will match .js and .jsx files
+        use: 'babel-loader', // Use babel-loader for JSX
+        exclude: /node_modules/, // Exclude node_modules folder
+      },
+      {
+        test: /\.tsx?$/,              // Handle .ts and .tsx files
         use: 'ts-loader',
         exclude: /node_modules/,
       },
@@ -24,5 +31,18 @@ module.exports = {
       },
     ],
   },
-  devtool: 'source-map',
+  devServer: {
+    // contentBase: './dist',            // Serve from the dist folder
+    static: {
+        directory: path.join(__dirname, 'dist'), // Serve files from the dist folder
+    },
+    hot: true,                        // Enable hot module replacement
+    open: true,                       // Open browser on server start
+    historyApiFallback: true,         // Allow client-side routing
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',  // HTML template
+    }),
+  ],
 };
